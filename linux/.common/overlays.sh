c26684c4e51700b4a62c285890676f8635e1410e
ADM <55254764+annadm99@users.noreply.github.com>
2025-01-09 17:16:29 +0100

    Update overlays.sh
    Signed-off-by: ADM <55254764+annadm99@users.noreply.github.com>

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa-pkg/radxa-overlays.git 04de4cab2c5eba53cb2caff940b78822dac50ab5
+    git_source https://github.com/annadm99/radxa-overlays 384c13db0038697ccdc960640553e3935f47e5ec
     cp -r $SCRIPT_DIR/.src/radxa-overlays/arch $TARGET_DIR
 }
